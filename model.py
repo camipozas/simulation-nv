a15f52878b614ba9dfcc018fd8d9db96e72b1a74
Camila Pozas <cpozasg1103@gmail.com>
2021-11-30 11:40:26 -0300

    not consider constaint 3

--- model.py
+++ model.py
@@ -21,23 +21,24 @@ def modelo(df_notas):
     model = LpProblem("Supply-Demand-Problem", LpMaximize)
 
     # Funcion Objetivo
-    model += (lpSum([cant_unidades[id_nota, id_producto] * df_notas.loc[(id_nota,id_producto),'precio_prod'].sum() for id_nota, id_producto in df_notas.index]))
+    model += (lpSum([cant_unidades[id_nota, id_producto] * df_notas.loc[(id_nota,id_producto),'precio_prod'].sum() for id_nota, id_producto in df_notas.index])), "obj"
 
     # Restricciones
     # (1) Inventario disponible:
-    model += lpSum([cant_unidades[id_nota, id_producto] - df_notas.loc[(id_nota, id_producto), 'inventario'].sum() for id_nota, id_producto in df_notas.index]) <= 0
+    model += lpSum([cant_unidades[id_nota, id_producto] - df_notas.loc[(id_nota, id_producto), 'inventario'].sum() for id_nota, id_producto in df_notas.index]) <= 0, "c1"
 
     # (2) La cantidad tiene que ser igual o menor a lo que solicitó el cliente:
-    model += lpSum([cant_unidades[id_nota, id_producto] - df_notas.loc[(id_nota, id_producto), 'cantidad'].sum() for id_nota, id_producto in df_notas.index]) <= 0
-    model += lpSum([cant_unidades[id_nota, id_producto] <=  nota_completa[id_nota, id_producto] * df_notas.loc[(id_nota, id_producto), 'cantidad'].sum() for id_nota, id_producto in df_notas.index]) 
-    model += lpSum([cant_unidades[id_nota, id_producto] <=  nota_modificada[id_nota, id_producto] * df_notas.loc[(id_nota, id_producto), 'cantidad'].sum() for id_nota, id_producto in df_notas.index]) 
+    model += lpSum([cant_unidades[id_nota, id_producto] - df_notas.loc[(id_nota, id_producto), 'cantidad'].sum() for id_nota, id_producto in df_notas.index]) <= 0, "c2"
+    model += lpSum([cant_unidades[id_nota, id_producto] <=  nota_completa[id_nota, id_producto] * df_notas.loc[(id_nota, id_producto), 'cantidad'].sum() for id_nota, id_producto in df_notas.index]), "c3"
+    model += lpSum([cant_unidades[id_nota, id_producto] <=  nota_modificada[id_nota, id_producto] * df_notas.loc[(id_nota, id_producto), 'cantidad'].sum() for id_nota, id_producto in df_notas.index]), "c4" 
 
     # (3) Si la nota de venta está completa entonces no se modifica:
-   # model += lpSum([nota_completa[id_nota, id_producto] <=  1 - nota_modificada[id_nota, id_producto] for id_nota, id_producto in df_notas.index]) 
+    #model += lpSum([nota_completa[id_nota, id_producto] <=  1 - nota_modificada[id_nota, id_producto] for id_nota, id_producto in df_notas.index]) 
 
     # (4)	Si la nota fue modificada, no se realiza nuevamente:
     #model += lpSum([nota_modificada[id_nota, id_producto] for id_nota, id_producto in df_notas.index]) == 1
     
     # Resuelve
+    print("status:", str(LpStatus[model.status]))
     model.solve()
-    LpStatus[model.status]
+    print("objetivo:", value(model.objective))
